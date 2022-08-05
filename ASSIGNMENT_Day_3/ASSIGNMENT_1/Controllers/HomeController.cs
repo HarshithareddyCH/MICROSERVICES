96937cfce6b9e6688a088ca1746a45c99e3ac093
HarshithareddyCH <97660439+HarshithareddyCH@users.noreply.github.com>
2022-08-05 11:50:10 +0530

    commit initial

---
+++ ASSIGNMENT_Day_3/ASSIGNMENT_1/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿Public Class HomeController
+    Inherits System.Web.Mvc.Controller
+
+    Function Index() As ActionResult
+        Return View()
+    End Function
+
+    Function About() As ActionResult
+        ViewData("Message") = "application description page."
+
+        Return View()
+    End Function
+
+    Function Contact() As ActionResult
+        ViewData("Message") = "contact page."
+
+        Return View()
+    End Function
+End Class
